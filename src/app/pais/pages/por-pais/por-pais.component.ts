1c792fd209b4a6eb88397d6eaf0d33ab98c0cfb1
Edsonjsf <edsonjsf1990@gmail.com>
2022-09-20 13:53:49 -0400

    Componente buscar por país

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [],
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  termino: string = '';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  buscar() {
+    console.log(this.termino);
+  }
 }
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
@@ -16,6 +17,6 @@ const importExport = [
 @NgModule({
   declarations: [importExport],
   exports: [importExport],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class PaisModule {}
